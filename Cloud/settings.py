daa67a0e4585d03b646b6a0d12ad343c90d12e1c
santhoshkumarr1096 <santhoshkumarr1096@gmail.com>
2017-11-01 10:04:27 +0530

    update

--- Cloud/settings.py
+++ Cloud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ekc+3&rg#=ymje6_mit4g5*anb+k=+n_hblwcglvw8fw&mwbl)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,4 +130,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
